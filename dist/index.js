11ed85fdd690ec031a88c2a49aef035a6e78d36c
ChinepohDivine-f <divinechinepoh@gmail>
2024-03-25 15:00:48 +0100

    typescript lessons first commit

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+var sales = 1234567;
+console.log(sales);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+let sales: number = 123_456_7
+console.log(sales);
+
+/// javascript types:
+// number
+// string
+// boolean
+// null
+// undefined
+// object
+    
+/// typescript additional types
+// any
+// unknown
+// never
+// enum
+// tuple
